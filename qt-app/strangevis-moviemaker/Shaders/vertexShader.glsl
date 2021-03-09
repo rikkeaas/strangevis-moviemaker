039ecdc81964d3caee7c61013a7a8a9c2062e190
rikke.aas <rikkke.aas@uib.no>
2021-03-09 10:16:55 +0100

    2D rendering of dataset

---
+++ qt-app/strangevis-moviemaker/Shaders/vertexShader.glsl
@@ -0,0 +1,8 @@
+uniform mat4 mvpMatrix;
+in vec4 vertex;
+out vec2 texCoords;
+void main(void)
+{
+	gl_Position = vertex;
+	texCoords = (vertex.xy + vec2(1.0)) * 0.5;
+}
--- qt-app/strangevis-moviemaker/main.cpp
+++ qt-app/strangevis-moviemaker/main.cpp
@@ -1,12 +1,25 @@
 #include "strangevismoviemaker.h"
+#include "Renderer.h"
+
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    Renderer* widget = new Renderer();
+
+    QMainWindow w;
+    w.setCentralWidget(widget);
+    w.resize(QSize(800, 600));
+    w.show();
+    return a.exec();
+
+    /*QApplication a(argc, argv);
     strangevismoviemaker w;
     w.setStyleSheet("background-color: #323232; color: white;");
     w.setWindowTitle("StrangeVis Movie Maker");
     w.show();
-    return a.exec();
+    return a.exec();*/
 }
