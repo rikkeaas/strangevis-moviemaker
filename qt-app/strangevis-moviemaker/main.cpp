54cb21f96389a574638686c31b33b04826a80817
Christian Hein <scheiehein@gmail.com>
2021-03-12 13:48:07 +0100

    changed layout to be generated programmatically

--- qt-app/strangevis-moviemaker/main.cpp
+++ qt-app/strangevis-moviemaker/main.cpp
@@ -8,9 +8,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Renderer* widget = new Renderer();
     strangevismoviemaker w;
-    w.setCentralWidget(widget);
     w.setStyleSheet("background-color: #323232; color: white;");
     w.setWindowTitle("StrangeVis Movie Maker");
     w.showMaximized();
